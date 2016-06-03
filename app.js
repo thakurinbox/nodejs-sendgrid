678c97593302bf46980da2ad772021d38129016e
System Administrator <root@Nareshs-MacBook-Pro.local>
2016-06-03 10:23:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+
+  var sendgrid_api_key = "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"
+  var sendgrid  = require('sendgrid')(sendgrid_api_key);
+
+  var email     = new sendgrid.Email({
+  to:       'thakurinbox@bgmail.com',
+  from:     'gencypher@gmail.com',
+  subject:  'Subject goes here',
+  text:     'Hello world'
+});
+sendgrid.send(email, function(err, json) {
+  if (err) { return console.error(err); }
+  console.log(json);
+});
+
+  res.send('Hello World!');
+
+
+
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+  console.log('Open http://localhost:3000/ on your browser!')
+});
